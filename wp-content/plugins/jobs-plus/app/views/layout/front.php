b8e2d3c95d0a4851ba072c695fc1bdba34567eea
Paul Jenkins <paul@turnpiece.com>
2015-01-16 17:06:08 +0000

    removing unused plugins

--- wp-content/plugins/jobs-plus/app/views/layout/front.php
+++
@@ -1,4 +0,0 @@
-<div class="hn-container">
-    <?php echo do_shortcode('<p style="text-align: center">[jbp-expert-post-btn][jbp-job-post-btn][jbp-expert-browse-btn][jbp-job-browse-btn][jbp-expert-profile-btn][jbp-my-job-btn]</p>'); ?>
-    <?php echo $content ?>
-</div>
