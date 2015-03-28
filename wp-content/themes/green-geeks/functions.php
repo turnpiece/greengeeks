9aef76289e0bb29724f943671f4ab000ad7f47db
Paul Jenkins <paul@turnpiece.com>
2015-03-28 11:57:38 +0000

    styling changes

--- wp-content/themes/green-geeks/functions.php
+++ wp-content/themes/green-geeks/functions.php
@@ -5,3 +5,12 @@
  * @package klein
  *
  */
+
+
+add_filter( 'comment_form_defaults', 'gg_remove_comment_form_allowed_tags' );
+function gg_remove_comment_form_allowed_tags( $defaults ) {
+
+	$defaults['comment_notes_after'] = '';
+	return $defaults;
+
+}
