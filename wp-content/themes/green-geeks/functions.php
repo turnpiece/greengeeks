8e750cc18a6bbd84572188c989469d3c82633bd5
Paul Jenkins <paul@turnpiece.com>
2015-04-02 17:21:12 +0100

    klein_setup function in child theme

--- wp-content/themes/green-geeks/functions.php
+++ wp-content/themes/green-geeks/functions.php
@@ -6,6 +6,55 @@
  *
  */
 
+if (!function_exists('klein_setup')):
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function klein_setup() {
+
+	/**
+	 * Make theme available for translation
+	 * Translations can be filed in the /languages/ directory
+	 * If you're building a theme based on klein, use a find and replace
+	 * to change 'klein' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'klein', get_template_directory() . '/languages' );
+	
+	// add default posts and comments RSS feed links to head
+	add_theme_support( 'automatic-feed-links' );
+
+	// WordPress 4.1 and above
+	add_theme_support('title-tag');
+	
+	// add featured-image theme support
+	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'klein-thumbnail-large', 750, 350, true ); 
+	add_image_size( 'klein-thumbnail-slider', 580, 277, true ); 
+	add_image_size( 'klein-thumbnail-highlights', 325, 325, true ); 
+	add_image_size( 'klein-thumbnail', 255, 185, true );
+	
+	// register the nav emnu
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', 'klein' ),
+		'top-links' => __( 'Top Links Menu', 'klein' ),
+	));
+	
+	// add post formats
+	add_theme_support( 'post-formats', array( 'video', 'gallery', 'quote', 'audio' ) );
+
+	// remove siebar settings
+	remove_action('admin_menu', 'klein_register_sidebar_settings');
+	
+}
+endif; // klein_setup
+
+add_action( 'after_setup_theme', 'klein_setup' );
+
 add_action( 'load-post.php', 'gg_post_meta_boxes_setup' );
 add_action( 'load-post-new.php', 'gg_post_meta_boxes_setup' );
 
