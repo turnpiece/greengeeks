69163765e11011b94aced892200e59a48997ccca
Paul Jenkins <paul@turnpiece.com>
2015-05-24 00:00:50 +0100

    BP security check plugin

---
+++ wp-content/plugins/bp-security-check/bp-security-check.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Plugin Name: BuddyPress Security Check
+ * Plugin URI:  http://bungeshea.com/plugins/bp-security-check/
+ * Description: Help combat spam registrations by forcing the user to answer a simple math sum while registering for your BuddyPress-powered site
+ * Author:      Shea Bunge
+ * Author URI:  http://bungeshea.com
+ * Version:     1.3.1
+ * License:     MIT
+ * License URI: http://opensource.org/licenses/MIT
+ * Text Domain: bp-security-check
+ * Domain Path: /languages/
+ */
+
+/**
+ * Adds a maths sum to the BuddyPress registration page that the user
+ * must answer correctly before registering
+ * @version 1.3.0
+ * @license http://opensource.org/licenses/MIT MIT
+ * @author Shea Bunge (http://bungeshea.com)
+ */
+
+/**
+ * Load up the localization file if we're using WordPress in a different language.
+ */
+function bp_security_check_load_textdomain() {
+	load_plugin_textdomain( 'bp-security-check', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'plugins_loaded', 'bp_security_check_load_textdomain' );
+
+/**
+ * Check if the user's input was correct
+ */
+function bp_security_check_validate() {
+	global $bp;
+
+	$uid = $_POST['bp-security-check-id'];
+	$sum = get_transient( 'bp-security-check_' . $uid );
+
+	$a  = $sum[0];
+	$op = $sum[1];
+	$b  = $sum[2];
+
+	$answer = intval( $_POST['bp-security-check'] );
+
+	/* Calculate the actual answer */
+	if ( 2 == $op ) {
+		$result = $a - $b;
+	} else {
+		$result = $a + $b;
+	}
+
+	/* The submitted answer was incorrect */
+	if ( $result !== $answer ) {
+		$bp->signup->errors['security_check'] = __( 'Sorry, please answer the question again', 'bp-security-check' );
+	}
+
+	/* The answer field wasn't filled in */
+	elseif ( empty( $answer ) ) {
+		$bp->signup->errors['security_check'] = __( 'This is a required field', 'bp-security-check' );
+	}
+
+	/* Clean up the transient if the answer was correct */
+	else {
+		delete_transient( 'bp-security-check' . $uid );
+	}
+}
+
+add_action( 'bp_signup_validate', 'bp_security_check_validate' );
+
+
+/**
+ * Render the input fields
+ */
+function bp_security_check_field() {
+
+	/* Get a random number between 0 and 10 (inclusive) */
+	$a = mt_rand( 0, 10 );
+	$b = mt_rand( 0, 10 );
+
+	/* Get a random operation */
+	$op = mt_rand( 1, 2 );
+
+	/* Make adjustments to the numbers for subtraction */
+	if ( 2 == $op ) {
+
+		/* Make sure that $a is greater then $b; if not, switch them */
+		if ( $b > $a ) {
+			$_a = $a;     // backup $a
+			$a = $b;      // assign $a (lower number) to $b (higher number)
+			$b = $_a;     // assign $b to the original $a
+			unset( $_a ); // destroy the backup variable
+		}
+
+		/* If the numbers are equal then the result will be zero, which will cause an error */
+		elseif ( $a == $b ) {
+			$a++;
+		}
+	}
+
+	/* Generate a unique ID to save the sum information under */
+	$uid = uniqid();
+
+	/* Save sum information (expiry = 12 hours) */
+	set_transient( 'bp-security-check_' . $uid, array( $a, $op, $b ), 12 * 60 * 60 );
+
+	?>
+	<div style="float: left; clear: left; width: 48%; margin: 12px 0;" class="security-question-section">
+		<h4><?php esc_html_e( 'Security Question', 'bp-security-check' ); ?></h4>
+		<?php do_action( 'bp_security_check_errors' ); ?>
+		<label for="bp-security-check" style="display: inline;">
+			<?php
+
+			/* First number */
+			echo $a;
+
+			/* Print operation as proper HTML entity */
+			if ( 2 == $op ) {
+				echo ' &#8722; '; // subtraction
+			} else {
+				echo ' &#43; '; // addition
+			}
+
+			/* Second number */
+			echo $b;
+
+			/* Equals symbol */
+			echo ' &#61;';
+
+			?>
+		</label>
+		<input type="hidden" name="bp-security-check-id" value="<?php echo $uid; ?>" />
+		<input type="number" name="bp-security-check" required="required" />
+	</div>
+	<?php
+}
+
+add_action( 'bp_after_signup_profile_fields', 'bp_security_check_field' );
---
+++ wp-content/plugins/bp-security-check/readme.txt
@@ -0,0 +1,82 @@
+=== BuddyPress Security Check ===
+Contributors: bungeshea
+Tags: math, registration, buddypress, security, anti-spam
+Tested up to: 4.1
+Stable tag: 1.3.1
+License: MIT
+License URI: http://opensource.org/licences/MIT
+Donate link: http://bungeshea.com/donate/
+
+Help combat spam registrations by forcing the user to answer a simple math sum while registering for your BuddyPress-powered site
+
+== Description ==
+
+This plugin will add a field to the BuddyPress registration form where the user will need to answer simple math sum before registering. This is an effort to stop spam bots from registering on your site. The math sum will be composed of adding or subtracting two random numbers between 0 and 10 (inclusive).
+
+You can learn more at the [plugin's website](http://bungeshea.com/plugins/bp-security-check/), or contribute to this plugin's development on [GitHub](https://github.com/bungeshea/bp-security-check)
+
+= Translations =
+
+Thanks to the awesome work of the following translators, this plugin can be used in these languages:
+
+* **Hungarian** thanks to Laszlo Espadas
+* **Brazilian Portuguese** thanks to Renato Alves
+* **Danish** thanks to Andreas Bjørn Hassing Nielsen
+* **Italian** thanks to [Nicole Curioni](http://nicolecurioni.com/)
+
+== Installation ==
+
+This plugin extends the functionality of [BuddyPress](http://wordpress.org/plugins/buddypress), which must be installed for this plugin to work
+
+1. Upload `bp-security-check.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the __(Plugins > Installed Plugins)__ menu in WordPress
+3. Done! The plugin has no settings to configure, just install and activate
+
+== Changelog ==
+
+= 1.3.1 =
+* Fixed incorrect term in Danish translation (Sikkerhedsspørgsmål is a single word) [[#](https://wordpress.org/support/topic/translation-293)]
+
+= 1.3.0 =
+* Added Danish translation by Andreas Bjørn Hassing Nielsen
+* Added Italian translation by [Nicole Curioni](http://nicolecurioni.com/)
+* Made transient names unique to prevent race conditions
+
+= 1.2.0 =
+* Added Hungarian translation by Laszlo Espadas
+* Added Brazilian Portuguese translation by Renato Alves
+* Saved sum information in database instead of hidden fields in an attempt to prevent bots
+* Code refactoring
+
+= 1.1.0.1 =
+* Fixed bug preventing the plugin from loading
+
+= 1.1.0 =
+* Updated to support translations
+* Use mt_rand() function instead of rand()
+* Add code documentation
+* Use proper class methods, not completely static
+* Ensure that the sum never equals 0
+
+= 1.0.1 =
+* Remove buggy multiplication and division functionality
+
+= 1.0.0 =
+* Stable version release
+
+== Upgrade Notice ==
+
+= 1.3.1 =
+Updated Danish translation
+
+= 1.2.0 =
+New translations plus fixes to prevent bots
+
+= 1.1.0.1 =
+Fixed bug preventing the plugin from loading
+
+= 1.1.0 =
+Updated to support translations
+
+= 1.0.1 =
+Quick patch to remove buggy multiplication and division functionality
